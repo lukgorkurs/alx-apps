fded6bfab0927f5e018a95d09ec4989f00d2e264
Łukasz Gorzołka <lukgorkurs@gmail.com>
2022-12-10 10:09:35 +0100

    1

---
+++ instagram firebase kopia/src/components/sections/Header/Header.js
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom'
+
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.logo}</h1>
+      <nav>
+        <ul>
+          <li>
+            {/* komponent Link od react-router-dom, rozni sie tym, ze zamiast atrybutu href jest atrybut to */}
+            <Link to="/add">
+              Dodaj nowy post
+            </Link>
+          </li>
+          <li>
+            <Link to="/login">
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/register">
+              Register
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
